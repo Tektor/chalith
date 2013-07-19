2a1ff68f6dba1ac73c1274d13c125b9d6dee6d82
Tektor <thaddel707@gmail.com>
2013-07-19 17:15:28 +0200

    recall uses material

--- tektor/minecraft/chalith/ChalithBase.java
+++ tektor/minecraft/chalith/ChalithBase.java
@@ -27,7 +27,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "Chalith", name = "Chalith", version = "0.1.1")
+@Mod(modid = "Chalith", name = "Chalith", version = "0.1.2")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class ChalithBase {
 
